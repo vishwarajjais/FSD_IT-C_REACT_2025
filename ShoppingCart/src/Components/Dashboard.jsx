d01b79f109fc85de47a6df8611e8cf88fe7aa269
Vishwaraj Jaiswal <vishwarajjaiswal64@gmail.com>
2025-11-24 13:09:48 +0530

    dashboard

--- ShoppingCart/src/Components/Dashboard.jsx
+++ ShoppingCart/src/Components/Dashboard.jsx
@@ -1,12 +1,46 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
 
 function Dashboard() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    async function getData() {
+      const response = await fetch("https://fakestoreapi.com/products");
+      const resData = await response.json();
+      setData(resData);
+    }
+    getData();
+  }, []);
+
+  function addToCart(ele) {
+    alert(ele.title)
+  }
+
   return (
     <div>
-      <h1 style={{backgroundColor:"brown", color:"white", padding:"0px"}}>Dashboard</h1>
-            
+      {data.length === 0 ? (
+        <h2>Data not found</h2>
+      ) : (
+        <div 
+          style={{
+            display: "grid",
+            gridTemplateColumns: "repeat(3, 1fr)",
+            gap: "20px"
+          }}
+        >
+          {data.map((ele) => (
+            <div style={{ border: "2px solid black", padding: "10px" }}>
+              <img src={ele.image} height={300} width={300} />
+              <h3>{ele.title}</h3>
+              <p>{ele.description}</p>
+              <h3>{ele.price}</h3>
+              <button onClick={() => addToCart()}>Add to cart</button>
+            </div>
+          ))}
+        </div>
+      )}
     </div>
-  )
+  );
 }
 
-export default Dashboard
+export default Dashboard; 
